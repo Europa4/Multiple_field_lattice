7f809a66867dbdda49caeb211d61d04df83ad9ce
Simon Woodward <Simonjwoodward5@gmail.com>
2019-11-13 18:03:20 +0000

    Actually started the simulation this time. Tightened up the thread control (reduced manual input)

--- devsource/main.cpp
+++ devsource/main.cpp
@@ -28,20 +28,26 @@ int main(int argc, char **argv)
   uniform_int_distribution<int> dist(0, pow(2,16));
 
   std::vector<int> powers = {4};
+  int start_record = 0;
+  int end_record = 200;
+  int cycles;
 
-  for(int i = 0; i < 200; ++i)
+  cycles = int(ceil((end_record - start_record)/world_size));
+
+  for(int i = 0; i < cycles; ++i)
   {
     seed = dist(rd);
-    printf("simulation %i initiated with seed %i \n", i*world_size + world_rank, seed + 7);
-    thimble_system sys(1, 10, .4, seed);
+    printf("simulation %i initiated with seed %i \n", i*world_size + world_rank + start_record, seed);
+    thimble_system sys(1, 10, 1.5, seed);
     sys.add_scalar_field(1.0);
     sys.add_scalar_field(3.0);
     //sys.add_interaction(1./24, powers);
     sys.set_path("Data_matrix/");
-    sys.set_name("phi_" + std::to_string(i*world_size + world_rank));
+    sys.set_name("phi_" + std::to_string(i*world_size + world_rank + start_record));
     sys.set_dt(0.5);
+    sys.set_proposal_size(0.2);
     sys.simulate(pow(10, 3), pow(10, 5));
-    printf("simulation %i completed \n", i*world_size + world_rank + 7);
+    printf("simulation %i completed \n", i*world_size + world_rank + start_record);
   }
   
   MPI_Finalize(); //closing the MPI enviroment
