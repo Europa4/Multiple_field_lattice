66e3573325cd561dd2231cdad2cc06834d604ed8
Simon Woodward <Simonjwoodward5@gmail.com>
2019-12-20 17:00:01 +0000

    End of day commit 20/12/2019

--- devsource/Correlators/nk.py
+++ devsource/Correlators/nk.py
@@ -28,20 +28,22 @@ def jackknife(data, block_length = 250):
 def observable(phi_data, header, p):
     #This is the observable you want to jam into the equation 26 from the below arxiv link
     i_phi = np.reshape(phi_data, (prot.Nx, prot.Nrp))
-    i_phi = i_phi[:, :(prot.Nt - 2)]
+    i_phi = i_phi[:, :(prot.Nt - 1)]
+    init = np.reshape(header[1:(prot.Nx + 1)], (1, prot.Nx))
+    i_phi = np.concatenate((init, i_phi), axis = 1)
     phi_k = FT.ft(i_phi, p, prot.deltaX)
     phi_nk = FT.ft(i_phi, -1*p, prot.deltaX)
-    phi_k = np.reshape(phi_k, (1, prot.Nt - 2))
-    phi_nk = np.reshape(phi_nk, (prot.Nt - 2, 1))
+    phi_k = np.reshape(phi_k, (1, prot.Nt))
+    phi_nk = np.reshape(phi_nk, (prot.Nt, 1))
     propogator = np.matmul(phi_nk, phi_k)
     return propogator
 
-def calc_expectation_and_error(n):
-    expectation_observable = np.zeros((prot.Nt - 2, prot.Nt - 2), dtype = complex)
-    file_error = np.copy(expectation_observable)
+def calc_expectation_and_error(n_file):
+    expectation_observable = np.zeros((prot.Nt, prot.Nt), dtype = complex)
+    file_error = np.zeros(prot.Nt, dtype = complex)
     #data location
      
-    file_name = location + 'phi_' + str(n)
+    file_name = location + 'phi_' + str(n_file)
     #This is all just using pandas to load the CSV files, it's completely compatable with the outputs from the C++ code
     #It outputs it as a triplet of numpy arrays, header, aux_data, and phi_data
     data = pd.read_csv(file_name, header = None, skiprows = 1)
@@ -54,28 +56,34 @@ def calc_expectation_and_error(n):
     phi_data = temp[:, :, 0] + prot.j*temp[:, :, 1]
 
     number_of_iterations = phi_data.shape[0]
+    n = np.zeros((number_of_iterations, prot.Nt))
     #equation 26 in arxiv 1704.06404, |det J(0)| is ingored as it'll cancel between the numerator and the denominator
     phi_tilde = np.exp(aux_data[:, 2] + prot.j*aux_data[:, 3] - prot.j*aux_data[:, 1])
 
     denominator = np.mean(phi_tilde)
-    numerator = np.zeros((number_of_iterations, prot.Nt-2, prot.Nt-2), dtype = complex)
+    numerator = np.zeros((prot.Nt, prot.Nt), dtype = complex)
     p = 0
     for i in np.arange(int(number_of_iterations)):
         Obs = observable(phi_data[i, :], header, p)
-        numerator[i, :] = Obs*phi_tilde[i] #this is the unaveraged numerator
-    #This calculates the expression fully for one initiatlisation
-    expectation_observable = np.mean(numerator, axis = 0)/denominator
-
+        #This calculates the expression fully for one initiatlisation
+        F = np.diag(Obs)
+        print(numerator.shape)
+        ddF = FT.calc_ddF(Obs, prot.deltaT)
+        w_squared = ddF/F
+        c = np.sqrt(1 - 0.25*(prot.deltaT)**2*w_squared)
+        n[i, :] = c*np.sqrt(ddF*F) - 0.5
+        numerator = 
+        
     #error analysis for this file
-    for i in np.arange(prot.Nt - 2):
-        for j in np.arange(prot.Nt - 2):
-           file_error[i, j] = jackknife(np.real(numerator[:, i, j]/denominator), jackknife_block_length) + prot.j*jackknife(np.imag(numerator[:, i, j]/denominator), jackknife_block_length)
-    return [expectation_observable, file_error]
+    averaged_n = np.mean(n, axis = 0)
+    for i in np.arange(prot.Nt):
+        file_error[i] = jackknife(np.real(n[:, i]), jackknife_block_length) + prot.j*jackknife(np.imag(n[:, i]), jackknife_block_length)
+    return [averaged_n, file_error]
 
-n_files = 100
+n_files = 2
 jackknife_block_length = 250
-expectation_observable = np.zeros((n_files, prot.Nt - 2, prot.Nt - 2), dtype = complex)
-file_error = np.zeros((n_files, prot.Nt - 2, prot.Nt - 2), dtype = complex)
+expectation_observable = np.zeros((n_files, prot.Nt), dtype = complex)
+file_error = np.zeros((n_files, prot.Nt), dtype = complex)
 x_range = np.arange(prot.Nt) + 1
 n_threads = 2
 p = Pool(n_threads)
@@ -89,11 +97,3 @@ final_expectation = np.mean(expectation_observable, axis = 0)
 
 final_error = np.sqrt(np.sum(np.real(file_error)**2, axis = 0) + prot.j*np.sum(np.imag(file_error)**2, axis = 0))/np.sqrt(n_files)
 
-F = np.diag(final_expectation)
-ddF = FT.calc_ddF(final_expectation, prot.deltaT)
-
-w_squared = ddF/F
-c = np.sqrt(1 - 0.25*(prot.deltaT)**2*w_squared)
-n = c*np.sqrt(ddF*F) - 0.5
-
-print(n)
